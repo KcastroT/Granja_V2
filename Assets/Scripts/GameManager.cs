a87da5d08e3570df1e703bceeb7ed965269c81c9
KcastroT <a01798925@tec.mx>
2024-03-22 14:02:25 -0600

    Logica de inicio implementada

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,25 +1,64 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    //Este script contiene todas las variables y funciones globales del juego
-    public static GameManager Instance {get; private set;}
+    // Este script contiene todas las variables y funciones globales del juego
+    public static GameManager Instance { get; private set; }
     public int saldo;
+    public GameObject pantallaInicio;
+    public GameObject opcionesPanel;
+    public TMP_InputField nameInputField; // TMP_InputField instead of InputField
+
+    public GameObject HUD;
+    public string nombre; // Variable to store the input name
+
+    public string modoDeJuego;
 
     private void Awake()
     {
-        if(Instance == null)
+        if (Instance == null)
         {
             Instance = this;
-        }else{
-            Debug.Log("Hay más de un GameManager en escena!");
+        }
+        else
+        {
+            Debug.Log("¡Hay más de un GameManager en escena!");
         }
     }
 
-    
-    
-}   
+    public void Start()
+    {
+        StartCoroutine(ShowPantallaInicioWithDelay());
+    }
 
+    IEnumerator ShowPantallaInicioWithDelay()
+    {
+        yield return new WaitForSeconds(1.5f);
+        CargarPantalladeInicio();
+        yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Return));
+        nombre = nameInputField.text; // Store the input name
+        yield return new WaitForSeconds(1.5f);
+        MostrarOpcionesPanel();
+        HUD.SetActive(true);
+    }
 
+    public void CargarPantalladeInicio()
+    {   
+        HUD.SetActive(false);
+        pantallaInicio.SetActive(true);
+    }
+
+    public void MostrarOpcionesPanel()
+    {
+        pantallaInicio.SetActive(false);
+        opcionesPanel.SetActive(true);
+        
+    }
+
+    public void GetModoDeJuego(string modo)
+    {
+        modoDeJuego = modo;
+    }
+}
