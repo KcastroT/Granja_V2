517d4bf5749439811c5f64cb1e93a8e34c420139
Fabrizi0Martinez <a01749680@tec.mx>
2024-04-05 20:05:29 -0600

    BTN ciclo
    Botón de ciclado funcional

--- Assets/Scripts/WorldLight.cs
+++ Assets/Scripts/WorldLight.cs
@@ -4,22 +4,33 @@ using UnityEngine.Rendering.Universal;
 namespace WorldTime {
     [RequireComponent(typeof(Light2D))]
     public class WorldLight : MonoBehaviour {
-        public float duration = 5f;
+        public float duration = 1f;
         [SerializeField] private Gradient gradient;
         private Light2D _light;
         private float _startTime;
+        private bool _running;
 
         private void Awake() {
             _light = GetComponent<Light2D>();
-            _startTime = Time.time;
+            _running = false;
         }
 
         private void Update() {
-            float timeElapsed = Time.time - _startTime;
-            float percentage = Mathf.Sin(timeElapsed / duration * Mathf.PI);
-            percentage = Mathf.Clamp01(percentage);
 
-            _light.color = gradient.Evaluate(percentage);
+            if (_running) {
+                float timeElapsed = Time.time - _startTime;
+                float percentage = Mathf.Sin(timeElapsed / duration * Mathf.PI);
+                percentage = Mathf.Clamp01(percentage);
+                _light.color = gradient.Evaluate(percentage);
+            }
+            
         }
+
+        public void StartCycle() {
+            _startTime = Time.time;
+            _running = true;
+        }
+
+
     }
 }
