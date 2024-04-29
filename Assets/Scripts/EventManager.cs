fd2864fbca316c75e247ffaccca2339306fc5591
KcastroT <a01798925@tec.mx>
2024-04-29 09:11:10 -0600

    Mejoras de ortografía y randomness
    Hay randomness papums

--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -1,42 +1,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.UI;  // Necessary for UI manipulation
-using TMPro;  
-
+using UnityEngine.UI; // Necessary for UI manipulation
+using TMPro;
 
 public class EventManager : MonoBehaviour
 {
     public List<GameEvent> possibleEvents;
     public DialogTrigger dialogTrigger; // Reference to the DialogTrigger
-    
-    
 
-    
     public void TriggerRandomEvent()
     {
+        
         float totalWeight = 0f;
         foreach (GameEvent gameEvent in possibleEvents)
         {
             totalWeight += gameEvent.probabilityWeight;
         }
 
+        
+        if (possibleEvents.Count == 0 || totalWeight <= 0)
+        {
+            Debug.LogWarning("No events are available or all events have a weight of zero.");
+            return;
+        }
+
+        
         float randomPoint = Random.value * totalWeight;
         float cumulativeWeight = 0f;
+
+       
         foreach (GameEvent gameEvent in possibleEvents)
         {
             cumulativeWeight += gameEvent.probabilityWeight;
             if (randomPoint <= cumulativeWeight)
             {
+                
                 if (gameEvent.dialog != null)
                 {
-                    dialogTrigger.dialog = gameEvent.dialog; // Set the dialog in the trigger
-                    dialogTrigger.TriggerDialog(); // Trigger the dialog
-                    gameEvent.thisEvent.Invoke(); // Invoke the event
+                    dialogTrigger.dialog = gameEvent.dialog; 
+                    dialogTrigger.TriggerDialog(); 
                 }
-                break;
+                gameEvent.thisEvent.Invoke(); 
+                return;
             }
         }
     }
 }
-
