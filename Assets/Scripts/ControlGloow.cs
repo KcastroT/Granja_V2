8e9b2d231c1917bb9d2afd332256d62eed50847f
Joel Vargas Reynoso <a01752464@tec.mx>
2024-04-30 12:31:51 -0600

    Reapply "falta manita, sobra tutorial"
    This reverts commit 24ff974294d2cc95a8ed9c701fa588b18fef1ba2.

---
+++ Assets/Scripts/ControlGloow.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlGloow : MonoBehaviour
+{
+    public List<ParticleSystem> gloowSystems;
+    private float tiempotranscurrido = 0;
+    private bool isPlaying = false;
+
+    void Start()
+    {
+        // Obtiene todos los componentes ParticleSystem en los hijos de este objeto
+        gloowSystems = new List<ParticleSystem>(GetComponentsInChildren<ParticleSystem>());
+        // Detiene todos los sistemas de partículas desde el inicio
+        foreach (var system in gloowSystems)
+        {
+            system.Stop();
+        }
+    }
+
+    void Update()
+    {   
+        if (isPlaying)
+        {
+            tiempotranscurrido += Time.deltaTime;
+
+            if (tiempotranscurrido >= 5f)
+            {
+                foreach (var system in gloowSystems)
+                {
+                    system.Stop(); // Detiene cada sistema de partículas
+                }
+                isPlaying = false;
+                tiempotranscurrido = 0;
+            }
+        }
+    }
+
+    public void PlayGloow()
+    {   
+        foreach (var system in gloowSystems)
+        {
+            system.Play(); // Inicia cada sistema de partículas
+        }
+        isPlaying = true;
+        tiempotranscurrido = 0;
+    }
+}
