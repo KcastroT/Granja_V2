7ea2adc8df6640f7126c44aecbd69f1e3cd07be4
rogren <117366551+rogren@users.noreply.github.com>
2024-05-02 08:09:41 -0600

    terminado
    beats

--- Assets/Scripts/Tienda.cs
+++ Assets/Scripts/Tienda.cs
@@ -1,28 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; // Asegúrate de incluir este namespace para trabajar con elementos UI
 
 public class Tienda : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject MenuTienda;
-    
+    public GameObject MenuTienda;
+    private bool tiendaActiva = false;
 
-    public void OpenTienda()
+    // Referencias a los botones
+    public Button btnPausa;
+    public Button btnTienda;
+    public Button btnClock;
+    public GameObject btnCropMaizCanvas;
+    public GameObject btnCropCebadaCanvas;
+    public GameObject btnCropZanahoriaCanvas;
+    public GameObject btnCropTrigoCanvas;
+
+    public void ToggleTienda()
     {
-        // Activar el menú de la tienda
-        MenuTienda.SetActive(true);
-        
-        // Pausar el juego
-        Time.timeScale = 0;
+        tiendaActiva = !tiendaActiva;
+
+        if (tiendaActiva)
+        {
+            MenuTienda.SetActive(true);
+            Time.timeScale = 0; // Pausar el juego
+            SetButtonsInteractivity(false); // Desactiva los botones
+        }
+        else
+        {
+            MenuTienda.SetActive(false);
+            Time.timeScale = 1; // Reanudar el juego
+            SetButtonsInteractivity(true); // Reactiva los botones
+        }
     }
 
-    public void CloseTienda()
+     public void Continue()
     {
-        // Desactivar el menú de la tienda
+        tiendaActiva = false;
         MenuTienda.SetActive(false);
-        
-        // Reanudar el juego
         Time.timeScale = 1;
+    }
+    private void SetButtonsInteractivity(bool interactable)
+    {
+        // Establecer la interactividad de cada botón
+        btnPausa.interactable = interactable;
+        btnTienda.interactable = interactable;
+        btnClock.interactable = interactable;
+        // btnCropMaizCanvas.interactable = interactable;
+        // btnCropCebadaCanvas.interactable = interactable;
+        // btnCropZanahoriaCanvas.interactable = interactable;
+        // btnCropTrigoCanvas.interactable = interactable;
     }
 }
